bfe199d9bfe96409e9fc7a3b8d669fef9dbfa557
krivi <krivi95@hotmail.com>
2021-02-15 12:36:28 +0100

    Shared wallet contract

---
+++ Allowance.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.6.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol";
+
+contract Allowance is Ownable{
+    
+    using SafeMath for uint;
+    
+    mapping(address => uint) public allowance;
+    
+    event AllowanceChanged(address indexed _forWho, address indexed _fromWhom, uint _oldAmount, uint _newAmount);
+    
+    modifier ownerOrAllowed(uint _amount){
+        //Only owner or person who is given allowance (and has positive balance) can withdraw the money
+        require(isOwner() || allowance[msg.sender] >= _amount, "You are not allowed to withdraw the money!");
+        _;
+    }
+    
+    function isOwner() public view returns(bool) {
+        //Checking is account interacting with SC is owner of the account.
+        return msg.sender == owner();
+    }
+    
+    function contractBalance() public view returns(uint) {
+        //Returns balace of ether on this contract in Wei.
+        return address(this).balance;
+    }
+    
+    function addAllowance(address _to, uint _amount) public onlyOwner {
+        //Adding allowance to address (only owner can do that)
+        emit AllowanceChanged(_to, msg.sender, allowance[_to], _amount);
+        allowance[_to] = _amount;
+    }
+    
+    function reduceAllowance(address _from, uint _amount) internal {
+        emit AllowanceChanged(_from, msg.sender, allowance[_from], allowance[_from].sub(_amount));
+        allowance[_from] = allowance[_from].sub(_amount);
+    }
+}
---
+++ SharedWallet.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.6.0;
+
+import "./Allowance.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract SharedWallet is Allowance{
+    
+    //Events for balance changes
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyDeposited(address indexed from, uint _amount);
+    
+    function renounceOwnership() public override onlyOwner {
+        //Overriding function for renouncing ownership (function from Ownable contract)
+        revert("Can't renounce ownership of this funds!");
+    }
+    
+    receive() external payable {
+        //Fallback receive function for anyone to deposit the funds.
+        MoneyDeposited(msg.sender, msg.value);
+    }
+    
+    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
+        require(contractBalance() >= _amount, "There is not enough funds on the account!");
+        if(!isOwner()){
+            //For everyone who is given an allowance
+            reduceAllowance(msg.sender, _amount);
+        }
+        emit MoneySent(_to, _amount);
+        _to.transfer(_amount);
+    }
+}
